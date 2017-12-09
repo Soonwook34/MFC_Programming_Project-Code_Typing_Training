ffa9ed4b863bef23f3e35b0e6bdd8ddf7386fda8
순욱 박 <soonwook34@gmail.com>
2017-12-10 00:32:17 +0900

    긴글연습 임시 저장

---
+++ TypingTraining/LongCode/estimatedRTT1.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+int main() {
+int k, n = 100;
+double EstimatedRTT[n], SampleRTT[n], DevRTT[n], TimeoutInterval[n];
+int SampleSum = 0;
+double MeanRTT[n];
+double a = 0.875, b = 0.75;
+for(k=1; k<=n; k++) {
+if(k<=90)
+SampleRTT[k] = 1;
+else
+SampleRTT[k] = 2;
+}
+EstimatedRTT[1] = SampleRTT[1];
+DevRTT[1] = 0;
+TimeoutInterval[1] = EstimatedRTT[1] + 4 * DevRTT[1];
+SampleSum += SampleRTT[1];
+MeanRTT[1] = SampleSum/1;
+for(k=2; k<=n; k++) {
+EstimatedRTT[k] = a * EstimatedRTT[k-1] + (1-a) * SampleRTT[k];
+DevRTT[k] = b * DevRTT[k-1] + (1-b) * SampleRTT[k];
+TimeoutInterval[k] = EstimatedRTT[k] + 4 * DevRTT[k];
+SampleSum += SampleRTT[k];
+MeanRTT[k] = (double)SampleSum/(double)k;
+}
+printf("TimeoutInterval[91] = %f\n", TimeoutInterval[91]);
+printf("TimeoutInterval[100] = %f\n", TimeoutInterval[100]);
+printf("EstimatedRTT[91] = %f\n", EstimatedRTT[91]);
+printf("EstimatedRTT[100] = %f\n", EstimatedRTT[100]);
+printf("MeanRTT[91] = %f\n", MeanRTT[91]);
+printf("MeanRTT[100] = %f\n", MeanRTT[100]);
+return 0;
+}
---
+++ TypingTraining/LongCode/mergesort3.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+using namespace std;
+//합병 정렬 함수
+void mergeSort3(int *, int);
+//합병 함수
+void merge3(int *, int, int, int);
+//배열 출력 함수
+void printArray(int *, int);
+//메인 함수
+int main() {
+int n;
+//정렬할 원소의 수 받기
+cout<<"원소의 수 : ";
+cin>>n;
+//정렬할 배열 생성
+int a[n] = {0};
+//정렬할 원소 받기
+cout<<"원소 : ";
+for(int i=0; i<n; i++) {
+cin>>a[i];
+}
+//합병 정렬
+mergeSort3(a, n);
+cout<<"정렬 : ";
+//정렬된 배열 출력
+printArray(a, n);
+return 0;
+}
+//합병 정렬 함수
+void mergeSort3(int a[], int n) {
+//합병할 크기
+int size;
+//합병할 부분 배열의 low, middle, high
+int low, mid, high;
+//2개, 4개, ...씩 합병하면서 정렬
+for(size=1; size<n; size*=2) {
+//low, middle, high 설정 후 합병
+for(low=0; low<n; low+=size*2) {
+high = min(low+(size*2)-1, n-1);
+mid = min(low+size-1, n-1);
+merge3(a, low, mid, high);
+}
+}
+}
+//합병 함수
+void merge3(int a[], int low, int mid, int high) {
+int i, j, k;
+int temp[high-low+1] = {0};
+//i는 왼쪽, j는 오른쪽 부분 배열의 index, k는 임시 배열의 index
+i = low;
+j = mid+1;
+k = 0;
+//오름차순으로 합병하기
+while(i<=mid && j<=high) {
+if(a[i]<a[j]) {
+temp[k] = a[i];
+i++;
+}
+else {
+temp[k] = a[j];
+j++;
+}
+k++;
+}
+while(i<=mid) {
+temp[k] = a[i];
+i++;
+k++;
+}
+while(j<=high) {
+temp[k] = a[j];
+j++;
+k++;
+}
+//임시 배열에서 기존 배열로 복사
+for(i=low, k=0; k<high-low+1; i++, k++) {
+a[i] = temp[k];
+}
+}
+//배열 출력 함수
+void printArray(int a[], int n) {
+for(int i=0; i<n; i++) {
+cout<<a[i]<<" ";
+}
+cout<<endl;
+}
